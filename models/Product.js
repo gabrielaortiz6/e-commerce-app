df93ab79d601d142a26d91712640ecd0139da11d
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-04-22 20:37:06 -0400

    added model for product

--- models/Product.js
+++ models/Product.js
@@ -4,13 +4,42 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTERGER,
+      //SET DEFAULT, VALIDATE THAT VALUE IS NUMERIC
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      },
+    },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id',
+        }
+      }
+    },
   {
     sequelize,
     timestamps: false,
